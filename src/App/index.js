043ae17df4f5bcec8cd09121f1a9fb5d044dd3b3
Bryan Kegley <bjkegley@gmail.com>
2018-06-13 05:15:35 -0600

    Add apollo-react for graphql

--- src/App/index.js
+++ src/App/index.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react'
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
 
 class App extends Component {
   render() {
+    const { data } = this.props
     return (
       <div>
-        <p>Hello world!</p>
+        {data && data.loading && 
+          <div>Loading</div>
+        }
+        {data && !data.loading && data.getComments.map(comment => {
+          return (
+            <p key = {comment._id}>{comment.text}</p>
+          )
+        })
+        }
       </div>
     )
   }
 }
 
-export default App
+
+export default graphql(gql`
+{
+  getTodos {
+    todo
+    _id
+  }
+}
+`)(App)
